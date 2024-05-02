f1957dd2a30416555133807d81a9910af4cf90c2
Guney Unay <guneymarketing17@hotmail.com>
2024-05-02 21:15:16 +0300

    :rocket: inital comment

---
+++ src/components/badge/index.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import clsx from "clsx";
+export default function index({
+  title = "Label",
+  colors = "neutral",
+  size = "small",
+}) {
+  return (
+    <span
+      className={clsx(
+        "font-normal rounded-full inline-flex items-center justify-center ",
+        {
+          "text-xs px-1.5 py-0.5  ": size === "small",
+        },
+        {
+          "text-sm px-2 py-0.5": size === "medium",
+        },
+        {
+          "text-sm w-[56px] py-1 px-2.5": size === "large",
+        },
+        {
+          "border-spacing-0.5 border bg-gray-50 text-neutral-600":
+            colors === "neutral",
+        },
+        {
+          "border-red-200 border bg-red-50 text-red-600": colors === "error",
+        },
+        {
+          "border-amber-200 border bg-amber-50 text-amber-700":
+            colors === "warning",
+        },
+        {
+          "border-green-200 border bg-green-50 text-green-700":
+            colors === "success",
+        },
+        {
+          "border-indigo-200 border bg-indigo-50 text-indigo-700":
+            colors === "brand",
+        }
+      )}
+    >
+      {title}
+    </span>
+  );
+}
---
+++ src/utils/font.js
@@ -0,0 +1,2 @@
+import { Noto_Sans } from "next/font/google";
+export const mainFont = Noto_Sans({ subsets: ["latin"] });
