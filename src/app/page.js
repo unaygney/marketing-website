81cd32f2e685743299c9f27e7e5661c6168a44a2
Guney Unay <guneymarketing17@hotmail.com>
2024-05-07 21:30:41 +0300

    :lipstick: Update page

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,9 @@
+import Navbar from "@/components/navbar";
+
 export default function Home() {
   return (
-    <main className="w-full h-full flex items-center justify-center">test</main>
+    <main className="w-full h-full ">
+      <Navbar />
+    </main>
   );
 }
