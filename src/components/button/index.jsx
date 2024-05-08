e269dbc479d3d4dedf0c6f5272e13096a9836ef1
Guney Unay <guneymarketing17@hotmail.com>
2024-05-08 22:23:10 +0300

    :lipstick: button ui changes

--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -1,13 +1,19 @@
+"use client";
 import React from "react";
 import clsx from "clsx";
+import PropTypes from "prop-types";
+
 export default function Button({
   children,
   variant = "primary",
   size = "medium",
+  onClick,
+  className,
   ...props
 }) {
   return (
     <button
+      onClick={onClick}
       {...props}
       className={clsx(
         "rounded font-medium transition-colors duration-300 ease-in-out",
@@ -15,7 +21,6 @@ export default function Button({
           "inline-flex h-10  items-center  gap-1 px-[14px] text-[14px] ":
             size === "medium",
         },
-
         {
           " inline-flex h-11 items-center gap-1.5 px-4 text-base":
             size === "large",
@@ -29,22 +34,34 @@ export default function Button({
             size === "2xl-large",
         },
         {
-          "customs-shadow bg-indigo-700 text-white hover:bg-indigo-800  ":
+          "customs-shadow bg-indigo-700 text-white  hover:bg-indigo-800 focus:outline-indigo-200 disabled:bg-[#f5f5f5] disabled:text-neutral-400   ":
             variant === "primary",
         },
         {
-          "custom-shadow border-[0.5px] border-neutral-200 bg-white text-neutral-900 hover:bg-[#fafafa]  ":
+          "custom-shadow border-[0.5px] border-neutral-200 bg-white text-neutral-900 hover:bg-[#fafafa] focus:outline-indigo-200  disabled:border-none disabled:bg-[#f5f5f5] disabled:text-neutral-400 disabled:shadow-none":
             variant === "secondary",
         },
         {
-          " text-indigo-700 hover:bg-[#fafafa] ": variant === "tertiary",
+          " text-indigo-700 hover:bg-[#fafafa] focus:outline-indigo-200 disabled:bg-transparent  disabled:text-neutral-400 ":
+            variant === "tertiary",
         },
         {
-          "bg-red-600 text-white hover:bg-red-700 ": variant === "destructive",
+          "bg-red-600 text-white  hover:bg-red-700  focus:bg-red-700   focus:outline-red-200    disabled:bg-transparent disabled:text-neutral-400 ":
+            variant === "destructive",
         },
+        className,
       )}
     >
       {children}
     </button>
   );
 }
+
+// button props types
+Button.propTypes = {
+  children: PropTypes.node.isRequired,
+  variant: PropTypes.oneOf(["primary", "secondary", "tertiary", "destructive"]),
+  size: PropTypes.oneOf(["medium", "large", "extra-large", "2xl-large"]),
+  className: PropTypes.string,
+  onClick: PropTypes.func,
+};
