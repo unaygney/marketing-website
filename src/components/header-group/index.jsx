7387f3bcc8d81995f569ff78a1c0dba0912f8a38
Guney Unay <guneymarketing17@hotmail.com>
2024-05-17 20:27:33 +0300

    :lipstick: Created header-group component

---
+++ src/components/header-group/index.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import PropTypes from "prop-types";
+import clsx from "clsx";
+export default function HeaderGroup({
+  smallTitle,
+  mainTitle,
+  text,
+  className,
+}) {
+  return (
+    <section className={clsx("flex flex-col gap-3", className)}>
+      <h3 className="text-base font-semibold leading-6 text-indigo-700">
+        {smallTitle}
+      </h3>
+      <h4 className="text-3xl font-semibold leading-9 text-neutral-900 md:text-5xl ">
+        {mainTitle}
+      </h4>
+      <p className="mt-2 text-lg font-normal leading-7 text-neutral-600 md:text-xl">
+        {text}
+      </p>
+    </section>
+  );
+}
+
+HeaderGroup.propTypes = {
+  smallTitle: PropTypes.string.isRequired,
+  mainTitle: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+  className: PropTypes.string,
+};
