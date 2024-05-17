1a868ddffae5247aefc4a6a074ef6e5b6a5de11e
Guney Unay <guneymarketing17@hotmail.com>
2024-05-17 20:28:35 +0300

    :lipstick:  created teams component

---
+++ src/containers/main-container/premium-images/constant.js
@@ -0,0 +1,44 @@
+export const FEATURES = [
+  {
+    id: 0,
+    title: "Infinite Download",
+    content:
+      "Once you subscribe to our plans, they're all yours. Download as many as you want and use them for work presentations, wallpapers, and much more.",
+    icon: "/images/download.svg",
+  },
+  {
+    id: 1,
+    title: "Purely Handcrafted",
+    content:
+      "No AI, no generic images. Crafted from various chemicals, fabrics, clouds, or even particles as small as dust.",
+    icon: "/images/paint.svg",
+  },
+  {
+    id: 2,
+    title: "All Are Under lincensed",
+    content:
+      "The only limitation with these abstract images is that you are not able to sell them in any form, whether digital or hard copy (such as paintings or prints on paper).",
+    icon: "/images/copyright.svg",
+  },
+  {
+    id: 3,
+    title: "Cancel Anytime",
+    content:
+      "Subscribe at your own pace, and cancel when you feel it's enough.",
+    icon: "/images/cancel.svg",
+  },
+  {
+    id: 4,
+    title: "Empowering For Team",
+    content:
+      "We support multiple seats at once, requiring only a single payment.",
+    icon: "/images/public.svg",
+  },
+  {
+    id: 5,
+    title: "No Limitations",
+    content:
+      "Use as many as you want, from Dribbble presentations to PowerPoint presentations.",
+    icon: "/images/limits.svg",
+  },
+];
--- src/containers/main-container/teams/index.jsx
+++ src/containers/main-container/teams/index.jsx
@@ -2,7 +2,10 @@ import React from "react";
 import BrandSlider from "./brand-slider";
 export default function Teams() {
   return (
-    <section className="flex w-full flex-col items-center gap-8  bg-white py-12 md:py-16 xl:py-24 ">
+    <section
+      id="teams"
+      className="flex w-full flex-col items-center gap-8  bg-white py-12 md:py-16 xl:py-24 "
+    >
       <h2 className="text-base font-medium leading-6 text-neutral-600">
         Used by teams that you love
       </h2>
