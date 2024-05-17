2add07f27ed3051ee0571e36770a1af50dd55eea
Guney Unay <guneymarketing17@hotmail.com>
2024-05-17 20:27:52 +0300

    :lipstick: update main-container

--- src/containers/main-container/index.jsx
+++ src/containers/main-container/index.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Hero from "./hero";
 import Teams from "./teams";
+import PremiumImages from "./premium-images";
+
 export default function MainContainer() {
   return (
     <main className="h-full w-full rounded bg-white pb-10 shadow-lg ">
       <Hero />
       <Teams />
+      <PremiumImages />
     </main>
   );
 }
