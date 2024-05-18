ff49aa9ef4d23b080a3fa423e24dc0b3277fbb6f
Guney Unay <guneymarketing17@hotmail.com>
2024-05-18 13:38:58 +0300

    :lipstick: created Features

---
+++ src/containers/main-container/features/constant.js
@@ -0,0 +1,62 @@
+export const FEATURE_DATA = [
+  {
+    id: 0,
+    imageIsLeft: false,
+    FEATURES: [
+      {
+        id: 0,
+        title: "5K resolution support",
+        content:
+          "All images boast a minimum resolution of 5K, ensuring crisp, crystal-clear quality.",
+        icon: "/images/hd.svg",
+      },
+      {
+        id: 1,
+        title: "From water to glass",
+        content:
+          "We offer a wide array of abstractions, ranging from water to glass, and encompassing various styles including 3D and vector.",
+        icon: "/images/water.svg",
+      },
+      {
+        id: 2,
+        title: "Portrait or landscape",
+        content:
+          "Effortlessly adapt your images for any platform - whether it's a stunning wallpaper or captivating Instagram reels and stories.",
+        icon: "/images/portrait.svg",
+      },
+    ],
+    imageMobile: "/images/features-1-mobile.png",
+    imageTablet: "/images/features-1-tablet.png",
+    imageDesktop: "/images/features-1-desktop.png",
+  },
+  {
+    id: 1,
+    imageIsLeft: true,
+    FEATURES: [
+      {
+        id: 0,
+        title: "Faster downloads",
+        content:
+          "Our robust servers are primed to deliver the highest resolution images swiftly, ensuring a smooth download experience.",
+        icon: "/images/rocket.svg",
+      },
+      {
+        id: 1,
+        title: "Convenience for teams",
+        content:
+          "Your single account can accommodate multiple users simultaneously downloading without any disruptions, streamlining teamwork and productivity.",
+        icon: "/images/convenience.svg",
+      },
+      {
+        id: 2,
+        title: "Royalty-free licensing",
+        content:
+          "Our straightforward, royalty-free licensing means your chosen images are yours to innovate with, without the hassle of negotiating usage rights for every new project.",
+        icon: "/images/copyright.svg",
+      },
+    ],
+    imageMobile: "/images/features-2-mobile.png",
+    imageTablet: "/images/features-2-tablet.png",
+    imageDesktop: "/images/features-2-desktop.png",
+  },
+];
