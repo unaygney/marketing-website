e9e44efd0757108b7ae8b67c6c2077ae7a75f922
Guney Unay <guneymarketing17@hotmail.com>
2024-05-18 20:59:11 +0300

    :lipstick: created tab component

---
+++ src/containers/main-container/pricing/constant.js
@@ -0,0 +1,92 @@
+export const PRICING_DATA = {
+  monthly: {
+    plans: [
+      {
+        id: 0,
+        title: "Basic Plan",
+        isPopular: false,
+        description: "Access to a curated selection of absract images",
+        price: 9.99,
+        features: [
+          "Standard quality images",
+          "Limited to personal use",
+          "Email support",
+        ],
+      },
+      {
+        id: 1,
+        title: "Standad Plan",
+        isPopular: true,
+        description: "Next-level Integrations, priced economically",
+        price: 19.99,
+        features: [
+          "Expanded library with more diverse absract images",
+          "High-resolution images available",
+          "Suitable for commercial use",
+          "Priority email support",
+          "Advanced analytics",
+        ],
+      },
+      {
+        id: 1,
+        title: "Premium Plan",
+        isPopular: false,
+        description: "Experience limitless living for power users",
+        price: 29.99,
+        features: [
+          "Full access to the entire image library, including exclusive content",
+          "Highest quality images, including premium collections",
+          "Commercial and resale rights",
+          "Dedicated customer support line",
+          "24/7 support response time",
+          "Advanced analytics and insights",
+        ],
+      },
+    ],
+  },
+  annually: {
+    plans: [
+      {
+        id: 0,
+        title: "Basic Plan",
+        isPopular: false,
+        description: "Access to a curated selection of absract images",
+        price: 6.99,
+        features: [
+          "Standard quality images",
+          "Limited to personal use",
+          "Email support",
+        ],
+      },
+      {
+        id: 1,
+        title: "Standad Plan",
+        isPopular: true,
+        description: "Next-level Integrations, priced economically",
+        price: 15.99,
+        features: [
+          "Expanded library with more diverse absract images",
+          "High-resolution images available",
+          "Suitable for commercial use",
+          "Priority email support",
+          "Advanced analytics",
+        ],
+      },
+      {
+        id: 1,
+        title: "Premium Plan",
+        isPopular: false,
+        description: "Experience limitless living for power users",
+        price: 25.99,
+        features: [
+          "Full access to the entire image library, including exclusive content",
+          "Highest quality images, including premium collections",
+          "Commercial and resale rights",
+          "Dedicated customer support line",
+          "24/7 support response time",
+          "Advanced analytics and insights",
+        ],
+      },
+    ],
+  },
+};
--- src/containers/main-container/pricing/index.jsx
+++ src/containers/main-container/pricing/index.jsx
@@ -1,13 +1,15 @@
 "use client";
-import Button from "@/components/button";
-import HeaderGroup from "@/components/header-group";
-import Tabs from "@/components/tabs";
 import React from "react";
+import HeaderGroup from "@/components/header-group";
+import * as Tabs from "@radix-ui/react-tabs";
+import { PRICING_DATA } from "./constant";
+import Image from "next/image";
+import Button from "@/components/button";
 
 export default function Pricing() {
   return (
     <section
-      className="flex flex-col gap-10 bg-white  px-3 py-12 md:px-4 md:py-16  xl:p-24"
+      className="flex flex-col gap-10 bg-white px-3 py-12 md:px-4 md:py-16 xl:p-24"
       id="pricing"
     >
       <HeaderGroup
@@ -16,28 +18,89 @@ export default function Pricing() {
         text="Pick the plan that suits you today and step up as your demands grow - our flexible options have your journey mapped out."
         className="text-center"
       />
-      <Tabs>
-        {({ activeTab }) => (
+      <Tabs.Root defaultValue="monthly" orientation="vertical">
+        <Tabs.List aria-label="Pricing options" className="flex flex-col">
+          <Tabs.Trigger value="monthly" className="p-2">
+            Monthly
+          </Tabs.Trigger>
+          <Tabs.Trigger value="annually" className="p-2">
+            Annually
+          </Tabs.Trigger>
+        </Tabs.List>
+
+        <Tabs.Content value="monthly" className="flex flex-col gap-8 ">
           <>
-            <div className="flex items-center gap-4">
-              <Tabs.Tab
-                index={0}
-                className={`h-11 flex-1 rounded transition-shadow  delay-200 ${activeTab === 0 ? "shadow" : ""}`}
+            {PRICING_DATA.monthly.plans.map((plan) => (
+              <div
+                key={plan.id}
+                className="flex flex-col gap-8 rounded-lg border border-neutral-200 bg-white p-4 shadow-sm"
               >
-                Monthly
-              </Tabs.Tab>
-              <Tabs.Tab
-                index={1}
-                className={`h-11 flex-1 rounded transition-shadow delay-200  ${activeTab === 1 ? "shadow" : ""}`}
-              >
-                Annually
-              </Tabs.Tab>
-            </div>
-            <Tabs.TabPanel index={0}>Deneme1</Tabs.TabPanel>
-            <Tabs.TabPanel index={1}>Deneme2</Tabs.TabPanel>
+                <div>
+                  <h3 className="text-xl font-bold">{plan.title}</h3>
+                  <p className="mt-2">{plan.description}</p>
+                </div>
+                <div>
+                  <p className="mt-2 text-lg font-semibold">${plan.price}</p>
+                  <p>Billed annually(84$)</p>
+                </div>
+                <ul className="flex flex-col gap-5">
+                  {plan.features.map((feature, index) => (
+                    <li key={index}>{feature}</li>
+                  ))}
+                </ul>
+                <button>Buy Now</button>
+              </div>
+            ))}
           </>
-        )}
-      </Tabs>
+        </Tabs.Content>
+
+        <Tabs.Content value="annually" className="flex flex-col gap-8 ">
+          <>
+            {PRICING_DATA.annually.plans.map((plan) => (
+              <div
+                key={plan.id}
+                className="flex flex-col gap-8 rounded-lg border border-neutral-200 bg-white p-4 shadow-sm"
+              >
+                <div className="flex flex-col gap-2">
+                  <h3 className="text-2xl font-semibold leading-8 text-neutral-900 ">
+                    {plan.title}
+                  </h3>
+                  <p className="text-base font-normal leading-6 text-neutral-600">
+                    {plan.description}
+                  </p>
+                </div>
+                <div className="flex flex-col gap-2">
+                  <p className="mt-2 text-5xl font-semibold text-neutral-900">
+                    ${plan.price}
+                    <span className="text-base font-normal">/month</span>
+                  </p>
+                  <p className="text-base font-normal leading-6 text-neutral-600">
+                    Billed annually(84$)
+                  </p>
+                </div>
+                <ul className="flex flex-col gap-5">
+                  {plan.features.map((feature, index) => (
+                    <div className="flex items-center gap-3">
+                      <span className="inline-flex flex-shrink-0 items-center justify-center overflow-hidden rounded-full bg-indigo-50">
+                        <Image
+                          src="/images/check.svg"
+                          width={20}
+                          height={20}
+                          alt="check"
+                        />
+                      </span>
+                      <li key={index}>{feature}</li>
+                    </div>
+                  ))}
+                </ul>
+                <Button variant="secondary" size="normal" className={"h-12"}>
+                  Buy Now
+                </Button>
+              </div>
+            ))}
+          </>
+        </Tabs.Content>
+      </Tabs.Root>
     </section>
   );
 }
