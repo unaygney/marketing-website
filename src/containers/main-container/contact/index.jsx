91233f95361e84d4922a529a18a19f872b400f2c
Guney Unay <guneymarketing17@hotmail.com>
2024-05-22 18:52:23 +0300

    :lipstick: created contact component

---
+++ src/containers/main-container/contact/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Contact() {
+  return (
+    <section
+      id="contact"
+      className="flex   flex-col gap-10 bg-white px-3 py-12 md:px-4 md:py-16 xl:p-24"
+    >
+      Contact
+    </section>
+  );
+}
