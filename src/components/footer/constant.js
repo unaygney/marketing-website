8ce46d4d9c5da4c4a368a7b3e8cd30b9ae25310b
Guney Unay <guneymarketing17@hotmail.com>
2024-05-22 22:16:10 +0300

    :lipstick: created footer ui

---
+++ src/components/footer/constant.js
@@ -0,0 +1,45 @@
+export const FOOTER_DATA = [
+  {
+    id: 0,
+    title: "Features",
+    path: "/features",
+  },
+  {
+    id: 1,
+    title: "Pricing",
+    path: "/pricing",
+  },
+  {
+    id: 2,
+    title: "About Us",
+    path: "/about-us",
+  },
+  {
+    id: 3,
+    title: "Contact",
+    path: "/contact",
+  },
+];
+
+export const LOGOS = [
+  {
+    id: 0,
+    src: "/images/youtube.svg",
+  },
+  {
+    id: 1,
+    src: "/images/instagram.svg",
+  },
+  {
+    id: 2,
+    src: "/images/facebook.svg",
+  },
+  {
+    id: 3,
+    src: "/images/github.svg",
+  },
+  {
+    id: 4,
+    src: "/images/x.svg",
+  },
+];
--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,5 +1,35 @@
 import React from "react";
+import { getCurrentYear } from "@/utils/helpers";
+import { FOOTER_DATA, LOGOS } from "./constant";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Footer() {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="flex   w-full flex-col items-center gap-4  bg-white px-8 py-12">
+      <nav className="flex items-center gap-4">
+        {FOOTER_DATA.map(({ path, id, title }) => (
+          <Link
+            className="text-sm font-medium leading-5 text-neutral-600"
+            key={id}
+            href={path}
+          >
+            {title}
+          </Link>
+        ))}
+      </nav>
+
+      <ul className="flex gap-6">
+        {LOGOS.map(({ id, src }) => (
+          <li key={id}>
+            <Image src={src} alt="logo" width={24} height={24} />
+          </li>
+        ))}
+      </ul>
+
+      <span className="text-sm font-normal leading-5 text-neutral-900">
+        © {getCurrentYear()} Abstractly, Inc. All rights reserved.
+      </span>
+    </footer>
+  );
 }
