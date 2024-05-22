b4fa21ffc2d0e414ddee522e088ffc7333deef03
Guney Unay <guneymarketing17@hotmail.com>
2024-05-22 18:51:56 +0300

    :lipstick: update toastcontainer

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
 import Navbar from "@/components/navbar";
 import { mainFont } from "@/utils/font";
 import "./globals.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export const metadata = {
   title: "Marketing Website",
@@ -13,6 +15,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${mainFont.className} custom-background h-full w-full scroll-smooth p-4 antialiased`}
       >
+        <ToastContainer />
         <Navbar />
         {children}
       </body>
