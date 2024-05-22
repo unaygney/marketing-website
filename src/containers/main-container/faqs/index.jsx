2d26414caf51d70df4eebe2e644322f4fde3f98f
Guney Unay <guneymarketing17@hotmail.com>
2024-05-22 12:57:26 +0300

    :lipstick: update ui changes

--- src/containers/main-container/faqs/index.jsx
+++ src/containers/main-container/faqs/index.jsx
@@ -1,24 +1,19 @@
-"use client";
 import React from "react";
-import * as Accordion from "@radix-ui/react-accordion";
-import { FAQS_DATA } from "./constant";
+import Accordion from "./accordion";
+import HeaderGroup from "@/components/header-group";
+
 export default function Faqs() {
   return (
-    <section id="faqs">
-      <Accordion.Root>
-        {FAQS_DATA.map((faq) => (
-          <Accordion.Item value={`value-${faq.id}`} key={faq.id}>
-            <Accordion.Header>
-              <Accordion.Trigger>
-                <span>{faq.title}</span>
-              </Accordion.Trigger>
-            </Accordion.Header>
-            <Accordion.Content>
-              <p>{faq.content}</p>
-            </Accordion.Content>
-          </Accordion.Item>
-        ))}
-      </Accordion.Root>
+    <section
+      id="faqs"
+      className="flex flex-col gap-10 bg-white px-3 py-12 md:px-4 md:py-16 xl:p-24"
+    >
+      <HeaderGroup
+        mainTitle="Frequently asked questions"
+        text="Get all your questions answered"
+        className="text-center "
+      />
+      <Accordion />
     </section>
   );
 }
