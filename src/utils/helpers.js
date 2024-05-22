76a4b136df964f550b666e7d391a116c56ae8d17
Guney Unay <guneymarketing17@hotmail.com>
2024-05-22 22:17:04 +0300

    :hammer: update helpers.js

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -2,3 +2,7 @@ export const validateEmail = (email) => {
   const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return re.test(String(email).toLowerCase());
 };
+
+export const getCurrentYear = () => {
+  return new Date().getFullYear();
+};
